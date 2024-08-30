787d60e07ab58ea1428bc1b859599de87ee6a951
Pablo Chorda <pablochorda365@gmail.com>
2024-08-30 17:13:33 +0200

    Bloc de notas con JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,96 @@
-window.addEventListener("load", () =>{
+window.addEventListener("load", (event) => {
 
-    let numberAccount = document.querySelector(".header__number");
-    let money = document.querySelector(".header__money");
-    let containersIco = document.querySelectorAll(".item__ico");
-    let titles = document.querySelectorAll(".item__title");
-    let dates = document.querySelectorAll(".item__date");
-    let bills = document.querySelectorAll(".item__bill");
-    let totals = document.querySelectorAll(".item__total");
-    let bgLoads = document.querySelectorAll(".bg-load");
-    let bgLoadsText = document.querySelectorAll(".bg-load-text");
+    let btn = document.querySelector(".main__btn");
+    let notes = document.querySelector(".main__notes");
 
-    function setInfo(){
-        numberAccount.innerText = "ES04 3245 4452 7358 6778 0990";
-        money.innerText = "5.000€";
+    btn.addEventListener("click", (e) => {
+        // Agregar nota cuando doy click al boton
+        createNote("", "", true);
+    });
 
+    let createNote = (body = "", title = "", transition = true) => {
+        // Creando el html de la nota
+        let note = document.createElement('article');
+        note.classList.add("notes__note");
 
-        containersIco.forEach(ico => {
-            ico.innerHTML =`<i class="fa-regular fa-credit-card"></i>`;
+        note.innerHTML += `
+        <header class="note__header">
+            <input type="text" class="header__input" value="${title}" placeholder="Título">
+            <i class="note__icon fa-solid fa-trash"></i>
+        </header>
+
+        <textarea class="note__body" name="body" placeholder="Escribe aquí">${body}</textarea>
+        `;
+
+        // Elementos del DOM a utilizar
+        let input = note.querySelector(".header__input");
+        let textarea = note.querySelector(".note__body");
+        let trash = note.querySelector(".note__icon");
+
+        //Guardar datos de mi nota
+        input.addEventListener("input", () => {
+            update();
         });
 
-
-        titles.forEach(title => {
-            title.innerText = "Empresa";
+        textarea.addEventListener("input", () => {
+            update();
         });
 
-        dates.forEach(date=> {
-            date.innerText = "29/08/2024";
+        // Eliminar notas de la lista
+        trash.addEventListener("click", () => {
+            note.classList.remove("note--visible-no-transition");
+            note.classList.remove("note--visible"); 
+
+            setTimeout(() => {
+                note.remove();
+                update();
+            }, 310);
         });
 
-        bills.forEach(bill => {
-            bill.innerText = "-400€";
-        });
+        // Agrego nota al contenedor de notas
+        notes.appendChild(note);
 
-        totals.forEach(total => {
-            total.innerText = "5000€";
-        });
-
-        bgLoads.forEach(bgLoad => {
-            bgLoad.classList.remove("bg-load");
-        });
-
-        bgLoadsText.forEach(bgLoadText => {
-            bgLoadText.classList.remove("bg-load-text");
-        });
-
+        if (transition) {
+            setTimeout(() => {
+                note.classList.add("note--visible");            
+            }, 10);            
+        } else {
+            note.classList.add("note--visible-no-transition"); 
+        }
     }
 
-    setTimeout(setInfo, 3000);
-    //setInfo();
+    let update = () => {
+        notesArray = [];
+        let notesDom = document.querySelectorAll(".notes__note");
+
+        notesDom.forEach(noteDom => {
+            let noteInput = noteDom.querySelector(".header__input");
+            let noteTextarea = noteDom.querySelector(".note__body");
+
+            let note = { 
+                "body": noteTextarea.value, 
+                "title": noteInput.value
+            };
+
+            if (note.body.trim() != "" || note.title.trim() != "") {
+                notesArray.push(note);                
+            }
+        });
+
+        // Persistir notas
+        localStorage.setItem("notes", JSON.stringify(notesArray));
+    }
+
+    // Función para que se listen las notas de nuevo
+    let getNotes = () => {
+        let notesStorage = JSON.parse(localStorage.getItem("notes"));
+
+        if (notesStorage) {
+            notesStorage.forEach(note => {
+                createNote(note.body, note.title, false);
+            });
+        }
+    }
+
+    getNotes();
 });
